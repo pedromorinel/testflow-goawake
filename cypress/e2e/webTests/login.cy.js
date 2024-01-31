1497d2f9e8b5972325924d66d4d7c5f6b8072de4
pedroca <pedrocarvalhomorinel101@gmail.com>
2024-01-31 16:04:46 -0300

    sz

--- cypress/e2e/webTests/login.cy.js
+++ cypress/e2e/webTests/login.cy.js
@@ -1,14 +1,15 @@
 import { goAwakeLogin} from "../pages/GoAwakeLinksPO";
-import loginPage from "../pages/GoAwakeLoginPagePO";
+import loginPages from "../pages/GoAwakeLoginPagePO";
 import { google } from 'googleapis';
 
 describe('Login', () => {
   it('passes', () => {
+    const goAwakeLoginPage = loginPages.goAwakeLoginPage();
     goAwakeLogin();
     cy.wait(5000)
-    cy.xpath(loginPage.selectors.usernameInput).type("p.morinel");
-    cy.xpath(loginPage.selectors.passwordInput).type("Creare4455.");
-    cy.xpath(loginPage.selectors.loginButton).click();
+    cy.xpath(goAwakeLoginPage.selectors.usernameInput).type("p.morinel");
+    cy.xpath(goAwakeLoginPage.selectors.passwordInput).type("Creare4455.");
+    cy.xpath(goAwakeLoginPage.selectors.loginButton).click();
     const auth = new google.auth.OAuth2(
       'S41364301866-pcfmddldj4enela9bgjbjrvbd1m63rq0.apps.googleusercontent.com',
       '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',
