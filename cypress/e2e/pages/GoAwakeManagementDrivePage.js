93106c29816a520bb4137dcfa8b5d37a4b2cad1e
pedroca <pedrocarvalhomorinel101@gmail.com>
2024-02-06 14:36:38 -0300

    update

---
+++ cypress/e2e/pages/GoAwakeManagementDrivePage.js
@@ -0,0 +1,23 @@
+class driverPageObjects {
+    constructor() {
+        this.selectors = {}
+    }
+
+    driverObjects() {
+        this.selectors = {
+            newDriverButton: 'body > app-root > app-dashboard-layout > div > div.dashboard--content > mat-drawer-container > mat-drawer-content > div > app-drivers-list > cr-card > div > cr-card-title > div > cr-card-actions > div > button',
+            filterCustomer: '#customer > div > div.ui-dropdown-label-container > label',
+            inputCustomer: '#customer > div > div.ng-trigger.ng-trigger-overlayAnimation.ng-tns-c26-27.ui-dropdown-panel.ui-widget.ui-widget-content.ui-corner-all.ui-shadow.ng-star-inserted > div.ui-dropdown-filter-container.ng-tns-c26-27.ng-star-inserted > input',
+            selectCustomer: '#customer > div > div.ng-trigger.ng-trigger-overlayAnimation.ng-tns-c26-27.ui-dropdown-panel.ui-widget.ui-widget-content.ui-corner-all.ui-shadow.ng-star-inserted > div.ui-dropdown-items-wrapper > ul > p-dropdownitem > li',
+            editDriver: 'body > app-root > app-dashboard-layout > div > div.dashboard--content > mat-drawer-container > mat-drawer-content > div > app-drivers-list > cr-card > div > cr-card-body > div > p-table > div > div > table > tbody > tr:nth-child(5) > td:nth-child(9) > div > button:nth-child(2)',
+            nameInput: 'body > p-dynamicdialog > div.ng-trigger.ng-trigger-animation.ng-tns-c31-32.ui-dialog.ui-dynamicdialog.ui-widget.ui-widget-content.ui-corner-all.ui-shadow.ng-star-inserted > div.ui-dialog-content.ui-widget-content > driver-edit > div > div > input',
+            driverLicenceInput: 'body > p-dynamicdialog > div.ng-trigger.ng-trigger-animation.ng-tns-c31-32.ui-dialog.ui-dynamicdialog.ui-widget.ui-widget-content.ui-corner-all.ui-shadow.ng-star-inserted > div.ui-dialog-content.ui-widget-content > driver-edit > div > div > p-inputmask > input',
+            confirmEdit: 'body > p-dynamicdialog > div.ng-trigger.ng-trigger-animation.ng-tns-c31-32.ui-dialog.ui-dynamicdialog.ui-widget.ui-widget-content.ui-corner-all.ui-shadow.ng-star-inserted > div.ui-dialog-content.ui-widget-content > driver-edit > div > div > button',
+            driverId: 'body > app-root > app-dashboard-layout > div > div.dashboard--content > mat-drawer-container > mat-drawer-content > div > app-drivers-list > cr-card > div > cr-card-body > div > p-table > div > div > table > tbody > tr:nth-child(5) > td:nth-child(1) > div'
+        }
+        return this.selectors;
+    }
+
+}
+
+export default new driverPageObjects()
--- cypress/e2e/webTests/editInformations.cy.js
+++ cypress/e2e/webTests/editInformations.cy.js
@@ -1,6 +1,8 @@
 import { goAwakeGestao } from "../pages/GoAwakeLinks";
 import loginPages from "../pages/GoAwakeLoginPage";
 import managementObjects from "../pages/GoAwakeManagementHomePage";
+import driverPageObjects from "../pages/GoAwakeManagementDrivePage";
+import {generateRandomNumber} from "../resources/randomNumber";
 
 const managementLogin = loginPages.managementLoginPage();
 const navBar = managementObjects.navBar();
@@ -18,5 +20,20 @@ describe('Edit driver', () => {
         cy.get(navBar.menu).should('exist').click();
         cy.get(navBar.expandBar).click();
         cy.get(navBar.driverScreen).click();
+        cy.get(driverPage.filterCustomer).click();
+        cy.get(driverPage.inputCustomer).click().type('Creare Sistemas')
+        cy.get(driverPage.selectCustomer).click();
+        cy.get(driverPage.driverId).invoke('text').then((text) => {
+            const driverIdentification = text.trim();
+            cy.wrap(driverIdentification).as('driverIdentification');
+            cy.get(driverPage.editDriver).click();
+            cy.get(driverPage.nameInput).click().clear().type('Exemplo');
+            cy.intercept('PUT', 'https://api-qa.goawakecloud.com.br/api//v2/driver/'+driverIdentification).as('driverUpdate');
+            cy.get(driverPage.driverLicenceInput).click().clear().type(randomDriversLicence);
+            cy.get(driverPage.confirmEdit).click();
+            cy.wait ('@driverUpdate').then((interception) => {
+            expect(interception.response.statusCode).to.eq(200);
+            })
+        })
     })
 })
